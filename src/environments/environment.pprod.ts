3e097dff5a6302d8f40715caa8ed6cb2a33f638d
Ornice <ornihouss1@gmail.com>
2025-09-14 10:54:26 +0100

    fix data

---
+++ src/environments/environment.pprod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  API_SCHEME: 'https',
+  API_DOMAIN: 'mairiedangbo-api.exploitsweb.com/api',
+  API_FILE: 'mairiedangbo-api.exploitsweb.com/public',
+  API_VERSION: 'v1'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+
+export const environment = {
+  production: false,
+  isMockEnabled: false, // You have to switch this, when your real back-end is done
+  authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
+  API_SCHEME: 'http',
+  API_DOMAIN: 'localhost:8000/api',
+  API_FILE: 'localhost:8000',
+  API_VERSION: 'v1'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
